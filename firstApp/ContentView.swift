121efba90cfdb7ad33fc08dd52066c43ba5e008f
23ji <isj4194@naver.com>
2024-06-02 19:17:44 +0900

    if - 조건을 가지고 분기를 하는 조건문

--- firstApp/ContentView.swift
+++ firstApp/ContentView.swift
@@ -8,15 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    let name:[String]  = ["김본", "이상지", "윤재준"]
-    let nickName:[String:String] = ["Bon":"본드", "Sangji":"일찐짱", "Jaejun":"지도리"]
-    //let emoji:Set = ["🍠", "🍑", "🥥"]
-    var body: some View {
-        
-        VStack {
-            Text(name[0]+"(은)는 "+nickName["Bon"]!)
-            Text(name[1]+"(은)는 "+nickName["Sangji"]!)
-            Text(name[2]+"(은)는 "+nickName["Jaejun"]!)
+    
+    var randomNumber: Int = 17
+    var body: some View{
+        if (randomNumber % 2) == 0{
+            Text("짝수")
+        }else {
+            Text("홀수")
         }
     }
 }
